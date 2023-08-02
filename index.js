e2c5c84a226dbe4c07d7e2601946a4a01daa404b
Collins Kariuki <collinsmunkary@gmail.com>
2023-08-02 08:01:43 +0300

    Signing Cookies

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // Require the cookie parser
 const cookieParser = require('cookie-parser')
 // Execute cookieParser
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 
 app.get('/greet', (req,res) => {
@@ -17,6 +17,18 @@ app.get('/setname', (req,res) => {
     res.cookie('animal', 'harlequin shrimp')
     res.send('OK SENT YOU A COOKIE')
 })
+
+app.get('/getsignedcookie', (req,res) => {
+    res.cookie('fruit', 'grape', {signed: true})
+    res.send("OK SIGNED YOUR FRUIT COOKIE!")
+})
+
+app.get('/verifyfruit', (req,res) => {
+    console.log(req.cookies);
+    res.send(req.signedCookies)
+})
+
+
 
 
 app.listen(3000, () => {
